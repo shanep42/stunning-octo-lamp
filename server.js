29a46785d36fc0259281ba254a1c6d1fcb0351b3
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-09-30 18:21:11 -0500

    Add universal and POST /note routes, add fs module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Required packages
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 // Init express
@@ -12,12 +13,25 @@ const PORT = process.env.PORT || 3001;
 // TODO: Learn why normal routing (I think) doesn't seem to display properly - must be static?
 app.use(express.static('public'))
 
-// Set Notes route
+// Set Notes GET route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+// Notes POST route
+app.post('/api/notes', (req, res) => {
+    var note = req.body;
+    var noteArr = fs.readFileSync('./db/db.json');
+    noteArr.push(note);
+    fs.writeFileSync('.db/db.json', noteArr);
+    res.json(noteArr);
+    //TODO: Why is noteArr.push() "not a function?"
+})
 
+// Catch-all route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 // Listen on PORT
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
