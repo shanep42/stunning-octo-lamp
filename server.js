51ad302e44d47f36e101217d79e551aa6efd8124
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-09-30 18:01:07 -0500

    adds static and notes.html routes

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+// Required packages
 const express = require('express');
 const path = require('path');
 
+// Init express
 const app = express();
 
+// Set PORT/default PORT
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// Set static route
+// TODO: Learn why normal routing (I think) doesn't seem to display properly - must be static?
+app.use(express.static('public'))
 
+// Set Notes route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
+
+
+// Listen on PORT
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
